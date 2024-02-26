56eb4f319c58a9457facb3bbe5739f6225936073
Christoffer Fuglkjær <>
2024-02-26 12:32:32 +0100

    no message

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 from shiny.express import input, render, ui
 
-ui.input_numeric("a1", "a1", value="Hello, world!")
-ui.input_numeric("a2", "a2", value=True)
-ui.input_numeric("italic", "Italic", value=False)
+ui.page_opts(title="Clef project")
 
-ui.input_numeric("message", "Message", value="Hello, world!")
-ui.input_numeric("bold", "Bold", value=True)
-ui.input_numeric("italic", "Italic", value=False)
-ui.input_numeric("a1", "a1", value="Hello, world!")
-ui.input_numeric("a2", "a2", value=True)
-ui.input_numeric("italic", "Italic", value=False)
 
-ui.input_numeric("message", "Message", value="Hello, world!")
-ui.input_numeric("bold", "Bold", value=True)
-ui.input_numeric("italic", "Italic", value=False)
-ui.input_numeric("italic", "Italic", value=False)
+with ui.sidebar():
+    ui.input_numeric("Anteroposterior_1", "Anteroposterior_1", value="Hello, world!")
+    ui.input_numeric("Anteroposterior_2", "Anteroposterior_2", value=True)
+    ui.input_numeric("Vertical_1", "Vertical_1", value=False)
+
+    ui.input_numeric("Vertical_2", "Vertical_2", value="Hello, world!")
+    ui.input_numeric("Goslon_Score_A", "Goslon_Score_A", value=True)
+    ui.input_numeric("Total_Row_Score_A", "Total_Row_Score_A	", value=False)
 
 
 
 @render.ui
 def result():
     x = input.message()
-    if "Bold" in input.styles():
-        x = ui.strong(x)
+    if "Anteroposterior_1" is 2:
+        print(2)
     if "Italic" in input.styles():
         x = ui.em(x)
     return x
+
+
+
+# print("Anteroposterior_1", "Anteroposterior_1")
+# print("Anteroposterior_2", "Anteroposterior_2")
+# print("Vertical_1", "Vertical_1")
+# print("Vertical_2", "Vertical_2")
+# print("Goslon_Score_A", "Goslon_Score_A")
+# print("Total_Row_Score_A", "Total_Row_Score_A	")
+
+# print("Anteroposterior_1", "Anteroposterior_1")
+def main():
+    pass
